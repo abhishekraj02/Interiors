45d12d4d36f4618da984739ab22de1ac9adb85c1
Abhishek raj <abhishek.raj@wappotix.com>
2021-10-25 19:42:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="banner-img"></div>
+        <div className="banner-text">
+          <h1>Work in progress</h1>
+          <h2>Coming soon</h2>
+          <br />
+          <div>Mobile: 8297444416, 8297444417 </div>
+          <div>Email: associates@3istudio.in</div>
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
